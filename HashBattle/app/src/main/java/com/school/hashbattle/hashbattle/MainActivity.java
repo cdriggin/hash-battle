b1d1421769e1bad8ded062f6c3659a782082ebeb
Casey Riggin <krash.kon@gmail.com>
2016-05-31 18:07:38 -0700

    First Loading Page

--- HashBattle/app/src/main/java/com/school/hashbattle/hashbattle/MainActivity.java
+++ HashBattle/app/src/main/java/com/school/hashbattle/hashbattle/MainActivity.java
@@ -2,6 +2,7 @@ package com.school.hashbattle.hashbattle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //I feel like this is too simple, but my back stack seems to work properly.
+        //CDR
+
     }
 }
